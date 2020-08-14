55e860d6f6d7f9ade24f17d29af369cc67cfc6fd
Richard Strickland <richard.strickland@cbts.com>
2020-08-14 14:39:51 -0400

    token expiration added

--- generators/app/templates/api/auth/controller.js
+++ generators/app/templates/api/auth/controller.js
@@ -1,8 +1,10 @@
-import { sign } from '../../services/jwt'
-import { success } from '../../services/response/'
+import { sign } from "../../services/jwt";
+import { success } from "../../services/response/";
 
 export const login = ({ user }, res, next) =>
-  sign(user.id)
+  sign(user.id, {
+    expiresIn: process.env["TOKEN_EXP"],
+  })
     .then((token) => ({ token, user: user.view(true) }))
     .then(success(res, 201))
-    .catch(next)
+    .catch(next);
